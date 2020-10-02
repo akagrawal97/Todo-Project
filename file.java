5422bb90a6cbf8bed6cbce237fa686f0a95ffcd2
RAHUL SINHA <30353928+rahulsinha2911@users.noreply.github.com>
2020-10-02 18:32:25 +0530

    Create file.java

---
+++ file.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;  
+
+public class Todo_Spring 
+{ 
+
+    Scanner inFile = new Scanner(new FileReader("staffnumbers.txt"));
+
+    double Hours;
+    double Pay;
+
+    Hours = inFile.nextDouble();
+    Pay = inFile.nextDouble();
+    double earned = Length * Width;
+
+            System.out.println(earned);
+    }
